f285743e15b9cc84fa28d6d0eb92735401bf01eb
HongMinha <131649405+minhahamin@users.noreply.github.com>
2023-04-27 22:21:26 +0900

    [unrated] Title: 문자열 붙여서 출력하기, Time: 187.16 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        System.out.println (a.concat(b));  
+        
+        //문자열 붙이는 법
+        // 1) concat 사용 ex) System.out.println(문자열1.concat(문자열2))
+        // 2) + 사용
+        
+    }
+}

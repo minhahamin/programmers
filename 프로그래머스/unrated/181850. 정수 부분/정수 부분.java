f7adb3ada6bab899f379d5a67fd62b4a554f8fb5
HongMinha <131649405+minhahamin@users.noreply.github.com>
2023-04-29 18:24:40 +0900

    [unrated] Title: 정수 부분, Time: 0.19 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(double flo) {
+        int a;
+        
+        a=(int)flo;
+        System.out.println(a);
+        
+       return a;
+       
+    }
+}

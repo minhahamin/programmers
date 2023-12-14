651df9f7fa42f55723f586965d99f933d4a187cb
HongMinha <131649405+minhahamin@users.noreply.github.com>
2023-12-14 12:40:29 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int a, int b) {
+        String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] monthDays = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        int totalDays = 0;
+        for (int i = 0; i < a - 1; i++) {
+            totalDays += monthDays[i];
+        }
+        totalDays += b - 1; // 1월 1일부터 시작하므로 -1
+
+        return days[totalDays % 7];
+    }
+}
